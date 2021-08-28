e06290a06e67b2ca7f09e732500efd836c87a442
uccaybioralet <erdoganfurkan@gmail.com>
2021-08-28 09:28:05 +0100

    LoginPage created

---
+++ src/test/java/com/bleucrm/pages/LoginPage.java
@@ -0,0 +1,31 @@
+package com.bleucrm.pages;
+
+import com.bleucrm.utilities.ConfigurationReader;
+import com.bleucrm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(xpath = "//input[@name='USER_LOGIN']")
+    public WebElement usernameInput;
+
+    @FindBy(xpath = "//input[@name='USER_PASSWORD']")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//input[@class='login-btn']")
+    public WebElement loginButton;
+
+    public void loginAsHR(){
+        usernameInput.sendKeys(ConfigurationReader.get("hr_username"));
+        passwordInput.sendKeys(ConfigurationReader.get("hr_password"));
+        loginButton.click();
+    }
+
+
+}
