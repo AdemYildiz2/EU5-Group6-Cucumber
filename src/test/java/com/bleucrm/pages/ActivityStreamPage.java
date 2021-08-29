a03b6be2fe6cfe13465487f47d94fd7f097640cf
uccaybioralet <erdoganfurkan@gmail.com>
2021-08-29 21:21:25 +0100

    Click on tab method added

--- src/test/java/com/bleucrm/pages/ActivityStreamPage.java
+++ src/test/java/com/bleucrm/pages/ActivityStreamPage.java
@@ -1,4 +1,14 @@
 package com.bleucrm.pages;
 
+import com.bleucrm.utilities.Driver;
+import org.openqa.selenium.By;
+
 public class ActivityStreamPage extends BasePage{
+
+
+
+    public void clickOnTab(String tabName){
+        String tabID = "//*[@id='feed-add-post-blockblogPostForm']//*[.='"+tabName+"'][1]";
+        Driver.get().findElement(By.id(tabID)).click();
+    }
 }
